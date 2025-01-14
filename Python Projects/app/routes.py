d1247e80dc6bb63426facf5a8cd1227780567a11
Jepkor1r <jlagat007@gmail.com>
2025-01-14 21:44:30 +0300

    comments for understandability

--- Python Projects/app/routes.py
+++ Python Projects/app/routes.py
@@ -1,8 +1,17 @@
+# Import the Flask application instance `app` from the `app` package.
 
 from app import app
 
-@app.route('/')
-@app.route('/index')
+# We then use the route() decorator to tell Flask what URL should trigger our function.
+# Define a route using the `route()` decorator.
+# The decorator associates a URL path ('/' or '/index') with the `index` function below.
+
+@app.route('/')  # Maps the root URL (http://localhost:5000/) to this function.
+
+@app.route('/index') # Maps the URL http://localhost:5000/index to this function.
+
+# This function is executed when a user visits the specified routes.
+# It returns the string "Hello World!" to be displayed in the browser.
 
 def index():
     return "Hello World!"
