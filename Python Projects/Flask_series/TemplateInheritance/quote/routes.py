309591aeaa80aedd2dfe1ccd4b5c21b993c48645
Jepkor1r <jlagat007@gmail.com>
2025-07-04 09:22:47 +0300

    correclty pathed template folder

--- Python Projects/Flask_series/TemplateInheritance/quote/routes.py
+++ Python Projects/Flask_series/TemplateInheritance/quote/routes.py
@@ -1,7 +1,7 @@
 #Rendering means converting a template into a compete HTML page
 #render_template is a function in flask framework that takes a template filename and a variable list of template arguments, and returns the same template, but with all the placeholders in it replaced with actual values
 from flask import render_template
-from app import app
+from quote import app
 
 
 @app.route('/')  # Maps the root URL (http://localhost:5000/) to this function.
